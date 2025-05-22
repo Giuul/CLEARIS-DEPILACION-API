d85457ec5867875ee817bf50e5cd87e1838dab3d
Giuli :) <113950045+Giuul@users.noreply.github.com>
2025-05-22 09:32:54 -0300

    Agrego endpoints de prueba

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,8 +2,23 @@ import {Router} from "express"
 
 const router = Router()
 
-router.get("/users", (req, res) => {
-    res.send("obteniendo usuarios");
+router.get("/users/:id", (req, res) => {
+    const { id } = req.params;
+    res.send(`obteniendo usuario con id ${id}`);
 });
+
+router.post("/users", (req,res) =>{
+    res.send("Creando usuario")
+});
+
+router.put("/users/:id", (req, res) => {
+    const { id } = req.params;
+    res.send(`Actualizando libro con id: ${id}`);
+});
+
+router.delete("/users/:id", (req,res) =>{
+    const { id } = req.params;
+    res.send(`Borrando libro con id: ${id}`);
+})
 
 export default router;
